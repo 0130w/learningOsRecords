00f17f379dfc1ff0bf5e50b4e8fb62f98ff16dbb
0130w <1157374677@qq.com>
2023-04-09 14:13:15 +0800

    finish minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -2,16 +2,26 @@ use std::error::Error;
 use std::fs;
 use std::env;
 
-// I AM NOT DONE
-
 impl Config {
-    pub fn build(args : &Vec<String>) -> Result<Config, &'static str>{
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let ignore_case = env::var("IGNORE_CASE").map_or(true, |var| var.ne("0"));
+    pub fn build(mut args : impl Iterator<Item = String>) -> Result<Config, &'static str>{
+        args.next();
+        let query = match args.next(){
+            Some(arg) => arg,
+            None => return Err("Didn't get a query String."),
+        };
+        let file_path = match args.next() {
+            Some(path) => path,
+            None => return Err("Didn't get a file path."),
+        };
+
+        let ignore_case = match env::var("IGNORE_CASE") {Ok(value) => value.ne("0"), Err(_) => false};
+
+        let ignore_case = match ignore_case {
+            true => true,
+            false => {
+                args.next().map_or(false, |var| var.eq("ignore_case"))
+            }
+        };
 
         Ok(Config{ query, file_path, ignore_case })
     }
@@ -40,13 +50,7 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    for line in contents.lines() {
-        if line.contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn search_case_intensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,15 +6,13 @@ use minigrep::Config;
 // target : cargo run -- searchingString filename
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {println!("Problem passing arguments {err}"); process::exit(1);});
+    let config = Config::build(env::args()).unwrap_or_else(|err| {eprintln!("Problem passing arguments {err}"); process::exit(1);});
 
     println!("Searching for {}", config.query);
     println!("In path {}", config.file_path);
 
     if let Err(value) = minigrep::run(config){
-        println!("Application error: {value}");
+        eprintln!("Application error: {value}");
         process::exit(1);
     }
 }
