f527e19601d2e686ce3e87527d5ebddb72f2810f
0130w <1157374677@qq.com>
2023-05-06 08:38:14 +0800

    update README

---
+++ rust_test/rust_test/target/debug/deps/rust_test-666b7822acfadbec.d
@@ -0,0 +1,5 @@
+/Users/zzq/learningOsRecords/rust_test/rust_test/target/debug/deps/rust_test-666b7822acfadbec.rmeta: src/main.rs
+
+/Users/zzq/learningOsRecords/rust_test/rust_test/target/debug/deps/rust_test-666b7822acfadbec.d: src/main.rs
+
+src/main.rs:
---
+++ rust_test/rust_test/target/debug/deps/rust_test-df691e5a41235910.d
@@ -0,0 +1,5 @@
+/Users/zzq/learningOsRecords/rust_test/rust_test/target/debug/deps/rust_test-df691e5a41235910.rmeta: src/main.rs
+
+/Users/zzq/learningOsRecords/rust_test/rust_test/target/debug/deps/rust_test-df691e5a41235910.d: src/main.rs
+
+src/main.rs:
